7a29c302b68a0f7342d1f6bcdf39b2a89ae19b57
Janaka Premathilaka <janaka2@gmail.com>
2023-03-26 13:33:04 +0200

    Create Number.java

---
+++ src/main/java/com/reactive/numbers/model/Number.java
@@ -0,0 +1,13 @@
+package com.reactive.numbers.model;
+
+public class Number {
+    private final int value;
+
+    public Number(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
